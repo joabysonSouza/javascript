7f58fa960a33224e703a4e2d55d719e8edd0e0ef
joabysonSouza <125371904+joabysonSouza@users.noreply.github.com>
2023-04-24 11:32:47 -0300

    javascript
    mudei o codigo

--- exercicios3/cronometro/exercicios4/js.js
+++ exercicios3/cronometro/exercicios4/js.js
@@ -21,10 +21,39 @@ function inlista(n, l){
 
 function adicionar(){
     if(isNumero(num.value) && !inlista(num.value, valores)) {
-        alert('tudo certo')
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `valor ${num.value} foi adicionado`
+        lista.appendChild(item)
+        res.innerHTML=''
 
     }else {
         alert('valor invalido ou ja encontrado na lista ')
     }
-   
+     num.value = ''
+     num.focus()
+
 }
+ function finalizar(){
+    if(valores.length == 0 ){
+        alert('adicione valores na lista ')
+    }
+    else{
+        var total= valores.length
+        var maior = valores[0]
+        var menor = valores[0]
+        for(var pos in valores){
+            if(valores[pos] > maior)
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+
+            
+        }
+        res.innerHTML = ''
+        res.innerHTML += `<p>ao todo temos ${total} numeros cadrastados</p>`
+        res.innerHTML += `<p> o maior valor foi ${maior}<p>`
+        res.innerHTML += `<p> o menor valor foi ${menor}<p>`
+
+    }
+ }
---
+++ exercicios3/cronometro/exercicios4/teste/script.js
@@ -0,0 +1,13 @@
+  var n = document.querySelector('input#num').value
+    
+    
+    
+    function adicionar(n){
+        if( n > 100){
+            alert('numero invalido')
+
+        }else{
+            alert('tudo certo')
+        }
+
+    }
